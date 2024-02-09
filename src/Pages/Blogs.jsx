957a517ec71ee8a23a6c061d731dbebf7c164fb3
xbot developer <xbotdeveloper@gmail.com>
2024-02-09 18:41:54 +0100

    first commit

---
+++ src/Pages/Blogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BlogsComp from '../Components/Blogs/BlogsComp'
+
+function Blogs() {
+  return (
+    <div className='min-h-[650px] pt-20 bg-white'>
+      <BlogsComp />
+    </div>
+  )
+}
+
+export default Blogs
